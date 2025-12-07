bb42fb8376c16a256125826dc23016dbfaf98c67
Chris Morrow <cmorrow181@gmail.com>
2025-12-07 09:54:36 -0500

    force node v20

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,26 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ["@nuxt/ui"],
   ui: {
     global: true,
-    icons: ['heroicons']
+    icons: ["heroicons"],
   },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   nitro: {
-    preset: 'static'
+    preset: "static",
   },
   app: {
     head: {
-      title: 'Mason Morrow - Theatre Educator, Performer, Playwright',
+      title: "Mason Morrow - Theatre Educator, Performer, Playwright",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Mason Morrow - Theatre Educator, Performer, Playwright' }
-      ]
-    }
-  }
-})
-
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Mason Morrow - Theatre Educator, Performer, Playwright",
+        },
+      ],
+    },
+  },
+});
